b07be6c80df500b5b5a57b66ad3c181f87b3d0d4
massooud <massoudoom407@gmail.com>
2024-10-02 12:13:44 +0300

    initial deployment changes

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-LHXBTAMU.mjs";import"./chunk-ZPU5RH76.mjs";import"./chunk-HONUNEG7.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
---
+++ server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{j as o,k as r,l as n}from"./chunk-ZPU5RH76.mjs";import{Zb as e}from"./chunk-HONUNEG7.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
